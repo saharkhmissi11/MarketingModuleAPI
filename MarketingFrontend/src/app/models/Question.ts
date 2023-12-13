4dae074b68e20307052ab6362059209ebde89e82
saharkhmissi11 <saharkhmissi90@gmail.com>
2023-12-13 19:10:09 +0100

    add api

--- MarketingFrontend/src/app/models/Question.ts
+++
@@ -1,6 +0,0 @@
-export interface Question {
-    id: number;
-    questionText: string;
-    options: string[];
-    questionnaire_id: number;
-  }
--- MarketingFrontend/src/app/models/Questionnaire.ts
+++
@@ -1,9 +0,0 @@
-import { Question } from "./Question";
-
-export interface Questionnaire {
-    id: number;
-    title: string;
-    description: string;
-    questions: Question[];
-    campaign_id: number;
-  }
